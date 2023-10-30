cb4277b603485b2333315cb06666c38ea686dc4f
konduruswetha <140582930+konduruswetha@users.noreply.github.com>
2023-10-30 12:08:20 +0530

    third commit

--- DemoPush/src/pack1/Welcome.java
+++ DemoPush/src/pack1/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		System.out.println("This is Swetha Konduru");
+		System.out.println("from git");
 	}
 
 }
